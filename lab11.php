19278b2cb2ad45f1993d60a9b342081cd6d0dac8
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-18 22:30:11 +0500

    lab11 practiced

---
+++ lab11.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is created using php scripting language</h1>
+    <?php
+    $fno=23;
+    $lno=21;
+
+    echo "The Sum is ".$fno ." and ".$lno." is this ".($fno+$lno)."<br>";
+    echo "The Difference is ".$fno ." and ".$lno." is this ".($fno-$lno)."<br>";
+    echo "The Product is ".$fno ." and ".$lno." is this ".($fno*$lno)."<br>";
+    echo "The Division is ".$fno ." and ".$lno." is this ".($fno/$lno)."<br>";
+
+
+
+    ?>
+    
+</body>
+</html>
